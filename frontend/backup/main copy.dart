618519ddd5be0d4577f40d8725873b28f733f9ee
adfinem <zenlaws02@gmail.com>
2025-03-10 13:51:57 +0800

    fix: moved the navigation to its own folder

--- frontend/backup/main copy.dart
+++ frontend/backup/main copy.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:techtags/screens/navigation_menu.dart';
+import 'package:techtags/screens/navigation/navigation_menu.dart';
 
 void main() {
   runApp(MaterialApp(
