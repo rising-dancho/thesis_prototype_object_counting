629a4c490330e7ee2bd6c62b0dd19583859afe29
adfinem <zenlaws02@gmail.com>
2025-03-25 03:27:35 +0800

    feat: increased the size of the photoviewer

--- frontend/lib/logic/tensorflow/photo_viewer.dart
+++ frontend/lib/logic/tensorflow/photo_viewer.dart
@@ -172,6 +172,7 @@ class _PhotoViewerState extends State<PhotoViewer> {
                   padding: EdgeInsets.symmetric(horizontal: 12, vertical: 6),
                   decoration: BoxDecoration(
                     color: Color.fromRGBO(0, 0, 0, 0.7),
+                    borderRadius: BorderRadius.circular(8),
                   ),
                   child: Text(
                     widget.titleController.text, // Display input text
