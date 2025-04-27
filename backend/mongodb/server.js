c7f11a9df03fe8532bb1703f3e2363c9316a820d
rising-dancho <zenlaws02@gmail.com>
2025-04-28 00:27:41 +0800

    fix: still fixing the sold and restock: DONE!

--- backend/mongodb/server.js
+++ backend/mongodb/server.js
@@ -458,13 +458,12 @@ app.post('/api/count_objects', async (req, res) => {
       });
     }
 
-    // ✅ THIS IS CALLED WHEN THE LABEL ALREADY EXISTS (NOT USING AUTO DETECT ADD MISSING LABEL): subtract `sold` from `availableStock` 
-    const updatedStock = await Stock.updateOne(
+    // ✅ THIS IS CALLED WHEN THE LABEL ALREADY EXISTS (NOT USING AUTO DETECT ADD MISSING LABEL): subtract `sold` from `availableStock`
+    await Stock.updateOne(
       { stockName: stockName },
       {
         $inc: {
           availableStock: -sold, // Subtract sold from available stock
-
           sold: sold, // Increase sold count
         },
       }
