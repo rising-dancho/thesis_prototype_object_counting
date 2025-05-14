8c90877e25d913729dbbe3da7a2d01871cb6ad2e
rising-dancho <zenlaws02@gmail.com>
2025-05-14 12:42:11 +0800

    feat: updated /api/update/sold, added a safety feature the prevent overwriting the whole table instead only updates what's being updated

--- backend/mongodb/server.js
+++ backend/mongodb/server.js
@@ -208,12 +208,13 @@ app.post('/api/update/sold', async (req, res) => {
   try {
     for (const stockItem of req.body) {
       await Stock.findOneAndUpdate(
-        { stockName: stockItem.stockName }, // Ensure correct search query
+        { stockName: stockItem.stockName },
         {
-          totalStock: stockItem.totalStock,
-
-          availableStock: stockItem.totalStock - (stockItem.sold ?? 0), // Ensure availableStock updates
-          unitPrice: stockItem.unitPrice ?? 0, // PRICE
+          $set: {
+            totalStock: stockItem.totalStock,
+            availableStock: stockItem.availableStock,
+            unitPrice: stockItem.unitPrice,
+          },
         },
         { upsert: true, new: true }
       );
