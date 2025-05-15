27e7ad58a434869dd33a53b45b74ae61247e1470
rising-dancho <zenlaws02@gmail.com>
2025-05-16 03:36:38 +0800

    feat: in progress, adding roles in the backend

--- backend/mongodb/schema/activity.js
+++ backend/mongodb/schema/activity.js
@@ -12,13 +12,14 @@ const activitySchema = new mongoose.Schema(
       type: mongoose.Schema.Types.ObjectId,
       ref: 'Stock',
       required: function () {
-        return ![
-          'Logged In',
-          'Registered',
-          'Updated User Role',
-          'Deleted User',
-        ].includes(this.action);
-      }, // ✅ Only require stockId for stock-related actions
+        const action = this.action || '';
+        return !(
+          action === 'Logged In' ||
+          action === 'Registered' ||
+          action.startsWith('Updated role of user') ||
+          action.startsWith('Deleted user')
+        );
+      },
     },
     countedAmount: { type: Number, default: 0 },
   },
